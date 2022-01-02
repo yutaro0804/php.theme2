24f4562552e6f93778215aec8463e1bb979eaaa4
youdbxa18 <youdbxa18@gmail.com>
2022-01-02 13:48:07 +0900

    Create form

---
+++ form.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Progate</title>
+  <link rel="stylesheet" type="text/css" href="stylesheet.css">
+  <link href='https://fonts.googleapis.com/css?family=Pacifico|Lato' rel='stylesheet' type='text/css'>
+</head>
+<body>
+  <div class="form">
+    <h2>入力フォーム</h2>
+    <form method="post"  action="display.php">
+      <textarea name="form"></textarea>
+      <input type="submit" value="送信">
+    </form>  
+  </div>
+</body>
+</html>
