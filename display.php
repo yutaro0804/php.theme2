c06590c83bd388b878f2a695f4b3a197843fb3c7
youdbxa18 <youdbxa18@gmail.com>
2022-01-02 13:37:23 +0900

    Create display

---
+++ display.php
@@ -0,0 +1,16 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Progate</title>
+  <link rel="stylesheet" type="text/css" href="stylesheet.css">
+  <link href='https://fonts.googleapis.com/css?family=Pacifico|Lato' rel='stylesheet' type='text/css'>
+</head>
+<body>
+  <div class="form">
+    <h2>出力</h2>
+    <?php echo  $_POST ?>
+  </div>
+</body>
+</html>
